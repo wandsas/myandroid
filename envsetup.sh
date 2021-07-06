3d006ca1c9080384eb04c4e6dffbbcdd6898cc1c
Sascha Wander <sascha.wander@gmail.com>
2021-07-06 09:46:41 +0000

    updated

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,6 @@
 export JAVA_HOME=${1:-/usr/lib/jvm/java-1.17.0-openjdk-amd64}
 export JAVA_OPTIONS="-Xms512m -Xmx1024m $JAVA_OPTIONS"
-
-# Android/AOSP
+# android/sdk
 if [ -d $HOME/android ]; then
     export ANDROID_HOME=$HOME/android/sdk
     PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
@@ -11,12 +10,14 @@ if [ -d $HOME/android ]; then
     PATH=$ANDROID_HOME/emulator:$PATH
     export PATH
 fi
-
-# The Chromium Projects
+# android-studio
+if [ -d $HOME/android/studio ]; then
+    export PATH=$HOME/android/studio:$PATH
+fi
+# chromium
 if [ -d $HOME/chromium/depot_tools ]; then
     export PATH=$HOME/chromium/depot_tools:$PATH
 fi
-
 JAVA_HOME=$JAVA_HOME
 JAVA_OPTIONS=$JAVA_OPTIONS
 ANDROID_HOME=$ANDROID_HOME
