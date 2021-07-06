a2ac7fc2615186b6707f236f0530fbd10cbf0382
Sascha Wander <sascha.wander@gmail.com>
2021-07-06 09:42:02 +0000

    bromite and vanadium

--- bromite.sh
+++ bromite.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 
-[ -d ~/chromium ] || mkdir -p ~/chromium && cd ~/chromium
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+[ -d ~/chromium ] || mkdir -p ~/chromium
+cd ~/chromium
+
+# depot_tools
+if [ ! -d ~/chromium/depot_tools ]; then
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+else
+    git pull origin master
+fi
 export PATH=$HOME/chromium/depot_tools:$PATH
 
+# bromite
+git clone git://github.com/bromite/bromite.git bromite
+cd bromite
 fetch --nohooks android
 cd src
 echo "target_os = [ 'android' ]" >> ../.gclient
@@ -13,12 +23,11 @@ build/install-build-deps-android.sh
 gclient runhooks
 gn args out/Default
 
-
 # get other build targets gn ls out/Default
 # //chrome/test:unit_tests use autoninja -C out/Default chrome/test:unit_tests
 autoninja -C out/Default chrome_public_apk
 
 
-git clone https://github.com/GrapheneOS/Vanadium.git vanadium
-cd vanadium
 
+### Bromite ###
+git clone git://github.com/bromite/bromite.git
--- studio.sh
+++ studio.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -d ~/android ] || mkdir -p ~/android
 cd ~/android
 curl -OL https://dl.google.com/dl/android/studio/ide-zips/4.0.1.0/android-studio-ide-193.6626763-linux.tar.gz
 echo 'f2f82744e735eae43fa018a77254c398a3bab5371f09973a37483014b73b7597 android-studio-ide-193.6626763-linux.tar.gz' | sha256sum -c
-tar xvf android-studio-ide-193.6626763-linux.tar.gz
+bsdtar xvf android-studio-ide-193.6626763-linux.tar.gz
 rm android-studio-ide-193.6626763-linux.tar.gz
 mv android-studio studio
 export PATH:$PATH:$HOME/android/studio/bin
