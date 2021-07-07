9bafc1506794b4531f26c13715cc10cce24a0365
Sascha Wander <sascha.wander@gmail.com>
2021-07-07 16:49:29 +0000

    prepare-vendor

--- android-prepare-vendor-files.sh
+++ android-prepare-vendor-files.sh
@@ -9,15 +9,15 @@ TARGET_BUILD_TYPE=release
 TARGET_PRODUCT=aosp_bramble
 TARGET_BUILD_VARIANT=user
 
-cd $HOME/grapheneos
-source script/envsetup.sh
+cd $GRAPHENEOS_HOME
+source $GRAPHENEOS_HOME/script/envsetup.sh
 choosecombo debug sdk_phone_x86 userdebug
 
 $GRAPHENEOS_HOME/vendor/android-prepare-vendor/execute-all.sh \
     -d $DEVICE \
     -b $BUILD_ID \
     -o $GRAPHENEOS_HOME/vendor/android-prepare-vendor
-mkdir -p vendor/google_devices
-rm -rf vendor/google_devices/$DEVICE
-mv vendor/android-prepare-vendor/$DEVICE/$BUILD_ID/vendor/google_devices/* vendor/google_devices/
+mkdir -p $GRAPHENEOS_HOME/vendor/google_devices
+rm -rf $GRAPHENEOS_HOME/vendor/google_devices/$DEVICE
+mv $GRAPHENEOS_HOME/vendor/android-prepare-vendor/$DEVICE/$BUILD_ID/vendor/google_devices/* $GRAPHENEOS_HOME/vendor/google_devices/
 
