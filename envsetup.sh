8be5ba66257b5a33246277997082ab7aa584ebca
Sascha Wander <sascha.wander@gmail.com>
2021-07-09 03:08:45 +0000

    foio

--- envsetup.sh
+++ envsetup.sh
@@ -4,6 +4,7 @@ export JAVA_OPTIONS="-Xms1024m -Xmx2048m -XX:-UsePerfData $JAVA_OPTIONS"
 export QT_DEBUG_PLUGINS=1
 # repo
 PATH=~/bin:$PATH
+
 # android/sdk
 if [ -d $HOME/android/sdk ]; then
     export ANDROID_HOME=$HOME/android/sdk
@@ -13,8 +14,9 @@ if [ -d $HOME/android/sdk ]; then
     PATH=$ANDROID_HOME/ndk-bundle:$PATH
     PATH=$ANDROID_HOME/emulator:$PATH
 fi
-if [ -d  "$HOME/android/sdk/ndk-bundle" ]; then
+if [ -d "$HOME/android/sdk/ndk-bundle" ]; then
   export ANDROID_SDK_ROOT=$HOME/android/sdk
+  export ANDROID_NDK_ROOT=$HOME/android/ndk
   export ANDROID_AVD_HOME=$HOME/.android/avd
   export LD_LIBRARY_PATH=$HOME/android/sdk/emulator/lib64:$LD_LIBRARY_PATH
   export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins
@@ -24,11 +26,13 @@ fi
 if [ -d $HOME/android/studio ]; then
     PATH=$HOME/android/studio/bin:$PATH
 fi
-# chromium
-if [ -d $HOME/chromium/depot_tools ]; then
-    PATH=$HOME/chromium/depot_tools:$PATH
+# chromium depot_tools
+if [ -d $HOME/android/depot_tools ]; then
+    PATH=$HOME/android/depot_tools:$PATH
 fi
 export PATH
+
+
 
 default_path () {
     PATH="/sbin:/bin:/usr/sbin:/usr/bin/:/usr/local/sbin:/usr/local/bin"
