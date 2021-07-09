e2df9c3b51ff9ae76fa3deac71455f14e4ea0748
Sascha Wander <sascha.wander@gmail.com>
2021-07-09 02:17:02 +0000

    foo

--- sdk.sh
+++ sdk.sh
@@ -20,6 +20,8 @@ if [ ! -f ~/bin/repo ]; then
 fi
 PATH=~/bin:$PATH
 
+[ -d $ANDROID_HOME ] || mkdir -p $ANDROID_HOME
+
 # 1. Install cmdline-tools
 [ -d $ANDROID_HOME/cmdline-tools ] || mkdir -p $ANDROID_HOME/cmdline-tools
 cd $ANDROID_HOME/cmdline-tools
@@ -27,7 +29,7 @@ curl -O https://dl.google.com/android/repository/commandlinetools-linux-7302050_
 echo '7a00faadc0864f78edd8f4908a629a46d622375cbe2e5814e82934aebecdb622 commandlinetools-7302050_latest.zip' | sha256sum -c
 bsdtar xvf commandlinetools-linux-7302050_latest.zip
 rm commandlinetools-linux-7302050_latest.zip
-mv tools latest
+mv cmdline-tools latest
 PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 sdkmanager 'cmdline-tools;latest'
 
