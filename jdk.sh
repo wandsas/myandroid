bc8ab6647ab2ef677cd87038fb4e2f50ff73df25
Sascha Wander <sascha.wander@gmail.com>
2021-07-13 22:34:05 +0000

    jdk

--- jdk.sh
+++ jdk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-curl -LO https://download.java.net/java/early_access/jdk17/30/GPL/openjdk-17-ea+30_linux-x64_bin.tar.gz
-curl -LO https://download.java.net/java/early_access/jdk17/30/GPL/openjdk-17-ea+30_linux-x64_bin.tar.gz.sha256
+curl -O https://download.java.net/java/early_access/jdk17/30/GPL/openjdk-17-ea+30_linux-x64_bin.tar.gz
+curl -O https://download.java.net/java/early_access/jdk17/30/GPL/openjdk-17-ea+30_linux-x64_bin.tar.gz.sha256
+echo $(cat openjdk-17-ea+30_linux-x64_bin.tar.gz.sha256) openjdk-17-ea+30_linux-x64_bin.tar.gz |
+  sha256sum -c
+bsdtar -xvf openjdk-17-ea+30_linux-x64_bin.tar.gz
+rm openjdk-17-ea+30_linux-x64_bin.tar.gz openjdk-17-ea+30_linux-x64_bin.tar.gz.sha256
+export JAVA_HOME=$HOME/build/jdk-17
+export PATH=$JAVA_HOME/bin:$PATH:$PATH
 
