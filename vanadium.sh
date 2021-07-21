a552f29cf124c911d1b5ee9ff5f606896efd84a5
crosshatcher <crosshatcher@onionmail.org>
2021-07-21 15:46:25 +0000

    vandium

--- vanadium.sh
+++ vanadium.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-
-. depot_tools.sh
+#!/usr/bin/env bash
 
 VANADIUM_HOME=$HOME/vanadium
 
-# vanadium
+# Check if depot_tools available. 
+. depot_tools.sh
+
+# Get vanadium source.
 if [ ! -d $VANADIUM_HOME ]; then
     git clone git://github.com/GrapheneOS/Vanadium.git $VANADIUM_HOME
 fi
@@ -16,7 +17,5 @@ gclient sync -D --with_branch_heads --with_tags --jobs 12
 build/install-build-deps-android.sh
 gclient runhooks
 gn args out/Default
-
-# get other build targets gn ls out/Default
-# //chrome/test:unit_tests use autoninja -C out/Default chrome/test:unit_tests
-# autoninja -C out/Default chrome_public_apk
+echo "Configure args.gn and start ninja:"
+echo "ninja -C out/Default/ trichrome_webview_64_32_apk trichrome_chrome_64_32_apk trichrome_library_64_32_apk"
