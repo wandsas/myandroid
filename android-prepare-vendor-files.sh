164fdc86b51d07b536ab55b627dd0ac2c23963d1
crosshatcher <crosshatcher@onionmail.org>
2021-07-26 06:56:28 +0000

    prepare-vendor-files

--- android-prepare-vendor-files.sh
+++ android-prepare-vendor-files.sh
@@ -10,12 +10,12 @@ BUILD_ID=rq3a.210605.005
 cd ~/grapheneos
 source script/envsetup.sh
 
-choosecombo release aosp_bramble user
+choosecombo release bramble user
 
 ~/grapheneos/vendor/android-prepare-vendor/execute-all.sh \
     -d $DEVICE -b $BUILD_ID -o ~/grapheneos/vendor/android-prepare-vendor
 mkdir -p ~/grapheneos/vendor/google_devices
 rm -rf ~/grapheneos/vendor/google_devices/$DEVICE
-mv ~/grapheneos/vendor/android-prepare-vendor/$DEVICE/$BUILD_ID/vendor/google_devices/*
-~/grapheneos/vendor/google_devices/
+mv ~/grapheneos/vendor/android-prepare-vendor/$DEVICE/$BUILD_ID/vendor/google_devices/* ~/grapheneos/vendor/google_devices/
+
 
