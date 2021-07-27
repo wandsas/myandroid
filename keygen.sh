b8cc2d3634c61740e9f73ca30cda7c65211c7a5e
crosshatcher <crosshatcher@onionmail.org>
2021-07-27 22:17:34 +0000

    keygen.sh

---
+++ keygen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Generate key
+keytool -genkey -v \
+    -keystore ~/.crosshatcher.keystore \
+    -storetype pkcs12 \
+    -alias k9mail \
+    -keyalg RSA \
+    -keysize 4096 \
+    -sigalg SHA512withRSA \
+    -validity 10000 \
+    -dname "cn=crosshatcher@onionmail.org"
+
+# Verify key
+keytool -export-cert \
+    -alias k9mail     \
+    -keystore */.crosshatcher.keystore | sha256sum
