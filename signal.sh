102f3f734ffbfe30c9edae86f7ebbe4c1215d17a
crosshatcher <crosshatcher@onionmail.org>
2021-08-04 05:11:27 +0000

    cleanup

--- signal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | \
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
