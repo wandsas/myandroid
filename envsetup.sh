66ee19334fe06b571f0f33a9bc61d91cc3219ac0
crosshatcher <crosshatcher@onionmail.com>
2021-08-18 19:06:51 +0000

    added OFFICIAL_BUILD=false

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,7 @@
 [ -f ~/.profile ] && . ~/.profile
 
 export GIT_SSH=ssh
+export OFFICIAL_BUILD=false
 
 openjdk8="/home/android/android/openlogic-openjdk-8u292-b10-linux-x64"
 openjdk11="/home/android/android/openlogic-openjdk-11.0.11+9-linux-x64"
